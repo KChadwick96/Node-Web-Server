18a896110b59991b2e748b7f93991d8d1ffe1d7a
Kieran Chadwick <kieran.chadwick@therobertstreethub.com>
2018-02-02 14:56:53 +0000

    using rewire in tests to check functions are called

--- spies/app.js
+++ spies/app.js
@@ -1,11 +1,11 @@
-const db = require('./app')
+const db = require('./db')
 
 module.exports.handleSignup = (email, password) => {
 
     // check email exists
 
     // add user to db
-    db.handleSignup({email, password})
+    db.saveUser({email, password})
 
     // send welcome message
 
--- spies/app.test.js
+++ spies/app.test.js
@@ -1,7 +1,15 @@
 const expect = require('expect')
 const sinon = require('sinon')
+const rewire = require('rewire')
+
+const app = rewire('./app')
 
 describe('App', () => {
+    const db = {
+        saveUser: sinon.spy()
+    }
+    app.__set__('db', db)
+
     it('should call the spy correctly', () => {
         const spy = sinon.spy()
 
@@ -10,5 +18,14 @@ describe('App', () => {
 
         sinon.assert.calledOnce(spy)
         sinon.assert.calledWith(spy, message)
+    })
+
+    it('should call saveUser with user object', () => {
+        const email = 'bob@outlook.com'
+        const password = 'abc123'
+
+        app.handleSignup(email, password)
+        sinon.assert.calledOnce(db.saveUser)
+        sinon.assert.calledWith(db.saveUser, {email, password})
     })
 })
