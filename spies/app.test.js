61e4ea702fcc29ba2d39393633be33817f060cb6
Kieran Chadwick <kieran.chadwick@therobertstreethub.com>
2018-02-02 14:27:54 +0000

    using sinon to determine if function is called

--- spies/app.test.js
+++ spies/app.test.js
@@ -1,10 +1,14 @@
-const jest = require('jest')
 const expect = require('expect')
+const sinon = require('sinon')
 
 describe('App', () => {
     it('should call the spy correctly', () => {
-        const spy = jest.fn()
-        spy()
-        //expect(spy).toHaveBeenCalled()
+        const spy = sinon.spy()
+
+        const message = 'hello'
+        spy(message)
+
+        sinon.assert.calledOnce(spy)
+        sinon.assert.calledWith(spy, message)
     })
 })
